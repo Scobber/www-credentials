0d56d178888c7c4c9e1dd7b1691bb83e86f1ee24
Scobber <scott@pep.id.au>
2021-12-16 12:23:45 +1100

    Update entrypoint.sh

--- obj/entrypoint.sh
+++ obj/entrypoint.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "$OPENSSL_DN$PROXY_WILDCARD_DOMAIN" -keyout /opt/privkey.pem -out /opt/fullchain.pem
+
 while true
 do
    echo Continuing Execution
